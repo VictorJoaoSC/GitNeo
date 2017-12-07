dc2e618ec1b388513115e6151301ff6edfc62a84
João Victor <victor.joaoVsJ@gmail.com>
2017-12-07 21:56:03 -0200

    Changes to be committed: 	modified:   Test.java

--- Test.java
+++ Test.java
@@ -1,11 +1,5 @@
 class Test{
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
-		System.out.println("Primeiro commit remoto!");
-		System.out.println("Primeira branch");
-=======
-		
->>>>>>> master
 	}
 }
